3e4abded6dcc86bf8ac44f6da4b2ed2dc270b0af
Hongyuan Zhang <zhygwu2016@gmail.com>
2019-11-16 16:31:11 -0800

    lc208 code method and test

---
+++ src/test/java/com/techbow/worksimulation/sensoriot/ingest/techbow/LeetCode208Test.java
@@ -0,0 +1,24 @@
+package com.techbow.worksimulation.sensoriot.ingest.techbow;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCode208Test {
+
+    private LeetCode208 lc208 = new LeetCode208();
+
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+    public void testTrie(){
+
+        // assertTrue();
+    }
+
+
+}
