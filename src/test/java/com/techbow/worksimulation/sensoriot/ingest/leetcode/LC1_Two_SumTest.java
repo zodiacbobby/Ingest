6448aaf20b2c55273169f890c70acdb96022f639
haoyul901209 <keithunightmare@gmail.com>
2019-11-16 16:34:02 -0800

    LC1 Test

--- src/test/java/com/techbow/worksimulation/sensoriot/ingest/leetcode/LC1_Two_SumTest.java
+++ src/test/java/com/techbow/worksimulation/sensoriot/ingest/leetcode/LC1_Two_SumTest.java
@@ -1,5 +1,32 @@
+package com.techbow.worksimulation.sensoriot.ingest.leetcode;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.any;
+import static reactor.core.publisher.Mono.when;
 
 class LC1_Two_SumTest {
+    private LC1_Two_Sum lc1;
 
+    @Before
+    public void init() {
+        lc1 = new LC1_Two_Sum();
+    }
+
+    @Test
+    public void testTwoSum() {
+
+     int[] testCase1 = {1, 2, 3, 4, 5};
+
+     //when(lc1.twoSum(anyArray())).thenReturn();
+
+
+     int testCase2 = 9;
+     int[] res = lc1.twoSum(testCase1, testCase2);
+     int[] expectation = {3, 4};
+     assertEquals(res, expectation);
+
+    }
 }
